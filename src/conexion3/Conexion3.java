caa4823c6c0935320c3a64683de3632e6e333f45
badenk4271 <ayavarav@gmail.com>
2017-11-10 20:11:29 -0300

    Importar clases

--- src/conexion3/Conexion3.java
+++ src/conexion3/Conexion3.java
@@ -24,7 +24,24 @@ import javax.swing.JFrame;
 public class Conexion3 
 {
     
+  
     
+    
+    
+    
+    
+    
+     public static void main(String[] args) 
+    {
+       Interfaz3 w = new Interfaz3 (); 
+       w.setVisible(true);
+       w.setBounds(0, 0, 400, 400);
+       w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+
+
+// TODO code application logic here
+    }
     
     
 }
