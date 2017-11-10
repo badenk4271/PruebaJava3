87b919ff454d90f3996d272c439b4366bbc50fb4
badenk4271 <ayavarav@gmail.com>
2017-11-10 20:49:21 -0300

    Se cambia la w. a ventana para poder salir el titulo de la  ventana

--- src/pruebajava3/PruebaJava3.java
+++ src/pruebajava3/PruebaJava3.java
@@ -28,11 +28,11 @@ public class PruebaJava3 {
      
     {
         Interfaz3 ventana = new Interfaz3();
-       Interfaz3 w = new Interfaz3 (); 
-       w.setVisible(true);
-       w.setBounds(0, 0, 400, 400);
+        
+        ventana.setVisible(true);
+       ventana.setBounds(0, 0, 400, 400);
        ventana.setTitle("Tercera evaluacion sumativa");
-       w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
 
 
